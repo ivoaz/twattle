567776ab4dce390545ecd6ae0fe2164d35c61898
Ivo Azirjans <ivo.azirjans@gmail.com>
2011-10-19 23:12:17 +0300

    added amakatanav's implementation of mood analyser

--- cli/determinate_mood.php
+++ cli/determinate_mood.php
@@ -3,6 +3,7 @@
 require_once(__DIR__.'/../app/bootstrap.php');
 
 use TweetEat\DependencyInjection\Container;
+use TweetEat\Analyser\MoodAnalyser;
 
 $container = new Container();
 
@@ -10,16 +11,17 @@ $database = $container->getDatabase();
 
 $collection = $database->getTweetCollection();
 
+$analyser = new MoodAnalyser($container['sys.root_dir'].'/data/analyser/mood_keywords.json');
+
 do {
     $tweets = $collection->findWithoutMood();
 
     foreach ($tweets as $tweet) {
         foreach ($tweet['subjects'] as $subject) {
-            // set random mood
             $subject['mood'] = array(
-                'value' => rand(-1, 1),
+                'value' => $analyser->analyseMood($tweet['original']['text'], 'en'),
             );
-            
+
             $collection->updateMood($tweet['_id'], $subject['_id'], $subject['mood']['value']);
         }
     }
