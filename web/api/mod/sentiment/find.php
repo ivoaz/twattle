ca14d72dc0d73a1e8a6f78cbcb641c051aeac13d
Ivo Azirjans <ivo.azirjans@gmail.com>
2011-11-07 23:03:28 +0200

    Added api for tweet sentiment moderation.

---
+++ web/api/mod/sentiment/find.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once(__DIR__.'../../../../../app/bootstrap.php');
+
+use TweetEat\DependencyInjection\Container;
+
+$container = new Container();
+
+$database = $container->getDatabase();
+$tweetColl = $database->getTweetCollection();
+
+// create criteria
+$criteria = array(
+    'moderation.sentiment' => array(
+        '$exists' => false,
+    ),
+);
+
+// get cursor
+$cursor = $tweetColl->collection->find($criteria, array(
+    '_id' => true,
+    'original.text' => true,
+));
+
+// sort results
+$cursor->sort(array(
+    '_id' => 1,
+));
+
+// limit results
+$limit = isset($_GET['limit']) ? $_GET['limit'] : 10;
+$cursor->limit($limit);
+
+// paginate results
+if (isset($_GET['page'])) {
+    $skip = $limit*($_GET['page']-1);
+    if ($skip < 0) $skip = 0;
+    $cursor->skip($skip);
+}
+
+// prepare for json
+$tweets = iterator_to_array($cursor);
+
+// return json
+echo json_encode($tweets);
---
+++ web/api/mod/sentiment/update.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once(__DIR__.'../../../../../app/bootstrap.php');
+
+use TweetEat\DependencyInjection\Container;
+
+$container = new Container();
+
+$database = $container->getDatabase();
+$tweetColl = $database->getTweetCollection();
+
+$tweet = $tweetColl->collection->findOne(array('_id' => $_POST['id']));
+var_dump($tweet['_id']);
+
+$tweetColl->collection->update(array(
+    '_id' => $_POST['id'],
+), array(
+    '$set' => array(
+        'moderation.sentiment' => (int)$_POST['rating'],
+    ),
+));
+
+echo json_encode(array('success' => true));
