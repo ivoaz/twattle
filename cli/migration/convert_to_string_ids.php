1e8226ebde7f6f138917d58a7fb965b0ef1ca938
Ivo Azirjans <ivo.azirjans@gmail.com>
2011-11-07 23:00:37 +0200

    Added float to string id migration script.

---
+++ cli/migration/convert_to_string_ids.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once(__DIR__.'/../../app/bootstrap.php');
+
+use TweetEat\DependencyInjection\Container;
+
+$container = new Container();
+
+$database = $container->getDatabase();
+
+// find tweets with float id
+$tweets = $database->getTweetCollection()->collection->find(array(
+    '_id' => array(
+        '$type' => 1
+    )
+));
+
+// WARNING, removes old document and inserts new one with string id
+foreach ($tweets as $tweet) {
+    $database->getTweetCollection()->remove($tweet['_id']);
+    $tweet['_id'] = isset($tweet['original']['id_str']) ? $tweet['original']['id_str'] : (string)$tweet['original']['id'];
+    $database->getTweetCollection()->insert($tweet);
+}
