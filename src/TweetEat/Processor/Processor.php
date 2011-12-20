0e3a30e8df2742d82dfc8a2609eb32613959c04d
trigator <azerspin@inbox.lv>
2011-12-20 18:38:31 +0200

    Pieliku valodas noteikšanu, bet varbūt vajadzētu tadjau to nolikt ka atsevišķu klasi?

--- src/TweetEat/Processor/Processor.php
+++ src/TweetEat/Processor/Processor.php
@@ -22,8 +22,17 @@ class Processor
      * @var SentimentAnalyser
      */
     protected $sentimentAnalyser;
-
+    
     /**
+    * @var engexp
+    */
+    protected $engexp
+    
+    /**
+    * @var notengexp
+    */
+    protected $notengexp
+    
      * @param Database $db
      * @param Normalizer $normalizer
      * @param ObjectDeterminator $objectDeterminator
@@ -34,6 +43,12 @@ class Processor
         $this->normalizer = $normalizer;
         $this->objectDeterminator = $objectDeterminator;
         $this->sentimentAnalyser = $sentimentAnalyser;
+        
+        $patterns = array(" this ", " that ", " other ", " is ",  " the ", " are ", " he ", " at ", " we ", " can ", " do ", " or ", " and ", " to ");
+        $this->engexp = '/(' .implode('|', $patterns) .')/i'; 
+        $badpatterns = array("ž", "č", "ķ", "ā",  " foi ", " de ", " se ", "ī", "õ", "ū", "ē", "ŗ", "ļ", " voy ", " als ", " ik " ,"é", "ć", " er ");
+        $this->notengexp = '/(' .implode('|',$badpatterns) .')/i'; 
+        
     }
 
     /**
@@ -48,10 +63,21 @@ class Processor
 
         $tweet['objects'] = $this->objectDeterminator->determine($tweet['normalized_text']);
         
+ 
         //eng check goes here
         if (empty($tweet['objects'])) {
             return false;
         }
+        
+        
+        if (!preg_match($this->engexp, $tweet['normalized_text'], $matches)) {
+            return false;
+        }
+       
+       if(preg_match($this->notengexp, $tweet['normalized_text'], $matches){
+            return false;
+       }
+
 
         $tweet['sentiment'] = $this->sentimentAnalyser->analyse($tweet['normalized_text']);
 
