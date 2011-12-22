1977988bfbe48b6ef1af86dc2a164e1e217e9cb5
Ivo Azirjans <ivo.azirjans@gmail.com>
2011-12-22 11:46:34 +0200

    Added Google Analytics

--- web/index.php
+++ web/index.php
@@ -114,5 +114,17 @@ foreach ($objects as $key => $object) {
                 <p>&copy; Twattle 2011</p>
             </footer>
         </div>
+
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-27889542-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </body>
 </html>
